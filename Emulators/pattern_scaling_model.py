7cb679062eb6e010f05f373c109134dcf27bbd46
Eric Pham <erpham@ucsd.edu>
2025-02-26 00:59:24 -0800

    edit ps model

--- Emulators/pattern_scaling_model.py
+++ Emulators/pattern_scaling_model.py
@@ -1,28 +1,20 @@
-import datetime as dt 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import netCDF4 as nc
 import xarray as xr
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.feature_selection import RFE
-from sklearn import metrics
 from sklearn.linear_model import LinearRegression 
-from sklearn.linear_model import Ridge
-
-from eofs.xarray import Eof
 from glob import glob
 
 data_path = "ClimateBench/" # Change to your working directory.
 
-# path to save the netcdf file
+# Path to save the netcdf file.
 path_output = data_path + 'outputs_ssp245_prediction_pattern2.nc'
 
-# path to testing data (ssp245)
+# Path to testing data (ssp245).
 pathssp245_Y = 'outputs_ssp245.nc'
 pathssp245_X = 'inputs_ssp245.nc'
 
-# Utilities for normalizing the emissions data
+# Utilities for normalizing the emissions data.
 min_co2 = 0.
 max_co2 = 2400
 def normalize_co2(data):
@@ -56,13 +48,17 @@ for input in inputs:
     weights = np.cos(np.deg2rad(X.latitude))
     
     axes[0, 0].plot(x, X['CO2'].data, label=label)
-    axes[0, 0].set_ylabel("Cumulative anthropogenic CO2 \nemissions since 1850 (GtCO2)")
+    axes[0, 0].set_ylabel(
+        "Cumulative anthropogenic CO2 \nemissions since 1850 (GtCO2)")
     axes[0, 1].plot(x, X['CH4'].data, label=label)
     axes[0, 1].set_ylabel("Anthropogenic CH4 \nemissions (GtCH4 / year)")
-    # FIXME: Not sure where this factor of 1000 comes from...! Maybe the CEDS data is really g/m-2/s?
-    axes[1, 0].plot(x, X['SO2'].weighted(weights).sum(['latitude', 'longitude']).data*SECONDS_IN_YEAR*1e-9, label=label)
+    # FIXME: Not sure where this factor of 1000 comes from...! 
+    # Maybe the CEDS data is really g/m-2/s?
+    axes[1, 0].plot(x, X['SO2'].weighted(weights).sum(
+        ['latitude', 'longitude']).data*SECONDS_IN_YEAR*1e-9, label=label)
     axes[1, 0].set_ylabel("Anthropogenic SO2 \nemissions (GtSO2 / year)")
-    axes[1, 1].plot(x, X['BC'].weighted(weights).sum(['latitude', 'longitude']).data*SECONDS_IN_YEAR*1e-9, label=label)
+    axes[1, 1].plot(x, X['BC'].weighted(weights).sum(
+        ['latitude', 'longitude']).data*SECONDS_IN_YEAR*1e-9, label=label)
     axes[1, 1].set_ylabel("Anthropogenic BC \nemissions (GtBC / year)")
 
 axes[0, 0].set_title('CO2')
@@ -72,13 +68,24 @@ axes[1, 1].set_title('BC')
 axes[0, 0].legend()
 plt.tight_layout()
 
-# Get one combined historical + ssp585 + ssp126 + ssp370 timeseries for now
-X = xr.concat([xr.open_dataset(data_path + 'inputs_historical.nc'), xr.open_dataset(data_path + 'inputs_ssp585.nc'),xr.open_dataset(data_path+ 'inputs_ssp126.nc'),xr.open_dataset(data_path+ 'inputs_ssp370.nc')], dim='time').compute()
+# Get one combined historical + ssp585 + ssp126 + ssp370 timeseries for now.
+X = xr.concat([
+    xr.open_dataset(data_path + 'inputs_historical.nc'), 
+    xr.open_dataset(data_path + 'inputs_ssp585.nc'),
+    xr.open_dataset(data_path + 'inputs_ssp126.nc'),
+    xr.open_dataset(data_path + 'inputs_ssp370.nc')], dim='time').compute()
 
-# Take the 2nd ensemble member for the historical (the first one has some missing DTR values for some reason...) and the 1st one for ssp585, ssp126, ssp370
-Y = xr.concat([xr.open_dataset(data_path + 'outputs_historical.nc').sel(member=2), xr.open_dataset(data_path + 'outputs_ssp585.nc').sel(member=1),xr.open_dataset(data_path+ 'outputs_ssp126.nc').sel(member=1),xr.open_dataset(data_path+ 'outputs_ssp370.nc').sel(member=1)], dim='time').compute()
+# Take the 2nd ensemble member for the historical (the first one has some 
+# missing DTR values for some reason...) and the 1st one for ssp585, ssp126, 
+# ssp370.
+Y = xr.concat([
+    xr.open_dataset(data_path + 'outputs_historical.nc').sel(member=2), 
+    xr.open_dataset(data_path + 'outputs_ssp585.nc').sel(member=1),
+    xr.open_dataset(data_path + 'outputs_ssp126.nc').sel(member=1),
+    xr.open_dataset(data_path + 'outputs_ssp370.nc').sel(member=1)], 
+    dim='time').compute()
 
-# Convert the precip values to mm/day
+# Convert the precip values to mm/day.
 Y["pr"] *= 86400
 Y["pr90"] *= 86400
 
@@ -90,12 +97,14 @@ global_mean_temp = Y['tas'].weighted(weights).mean(['lat', 'lon']).to_pandas()
 
 def get_rmse(truth, pred):
     weights = np.cos(np.deg2rad(truth.lat))
-    return np.sqrt(((truth-pred)**2).weighted(weights).mean(['lat', 'lon'])).data.mean()
+    return np.sqrt(((truth-pred)**2).weighted(weights).mean(
+        ['lat', 'lon'])).data.mean()
 
 y_inp_tas=Y["tas"].stack(dims=["lat", "lon"])
 y_inp_pr=Y["pr"].stack(dims=["lat", "lon"])
 y_inp_pr90=Y["pr90"].stack(dims=["lat", "lon"])
 y_inp_dtr=Y["diurnal_temperature_range"].stack(dims=["lat", "lon"])
+
 
 '''Pattern Scaling Model'''
 
